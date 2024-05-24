a1b887d4a2afab5618cde5d89d7fda280c97afd0
Ryan Decena <rpdecena@up.edu.ph>
2024-05-24 12:03:17 +0800

    fix: firebase configuration

--- app/firebase.tsx
+++ app/firebase.tsx
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYMkG_fXoxCRsKImpuWSHvSOZq_zv1fJU",
-  authDomain: "teddycare-12aaf.firebaseapp.com",
-  databaseURL: "https://teddycare-12aaf-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "teddycare-12aaf",
-  storageBucket: "teddycare-12aaf.appspot.com",
-  messagingSenderId: "196422363895",
-  appId: "1:196422363895:web:9210efa3e8fc77e62ec1f2",
-  measurementId: "G-BHFY68W6Q5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
