1987937ef1773e7c855108cf53a1693754d5a5b0
Ryan Decena <rpdecena@up.edu.ph>
2024-06-01 04:59:11 +0800

    feat: added home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
 // frontend
 import './page.css';
 import Navbar from './components/navbar';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import XIcon from '@mui/icons-material/X';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Contact from './assets/contact.png';
+import Shop from './assets/shop.png';
+import Storytelling from './assets/storytelling.png';
+import Features from './assets/features.png';
 
 import { useEffect, useState } from 'react';
 
@@ -32,38 +32,16 @@ export default function Home() {
       <Navbar isAuthenticated={userLogged} inTeddyCare={false} />
       <main>
         <section id='storytelling'>
-          <h1>Storytelling</h1>
+          <img src={Storytelling.src} alt="storytelling-section" />
+          <img src={Features.src} alt="features" />
         </section>
-
         <section id='shop'>
-          <h1>Shop</h1>
+          <img src={Shop.src} alt="shop-section" />
         </section>
-
-        <section id='contact' className='grid'>
-          <div className="contact-container">
-            <h3>Contact Us</h3>
-            <div>
-              <div className='email-container'>
-                <input type="text" placeholder='Subject'/>
-                <textarea name="message" id="message" placeholder='Message'></textarea>
-                <button className='send-button'>
-                  <p>Send</p>
-                </button>
-              </div>
-              <div className='contact-details-container'>
-                <p>Email: wecarebears0xE@gmail.com</p>
-                <div className="socmed-logo-container">
-                  <FacebookIcon className='icon'/>
-                  <InstagramIcon className='icon'/>
-                  <XIcon className='icon'/>
-                  <LinkedInIcon className='icon'/>
-                </div>
-              </div>
-            </div>
-          </div>
+        <section id='contact'>
+          <img src={Contact.src} alt="contact-section" />
         </section>
         <section id='about'>
-          <h1>About</h1>
         </section>
       </main>
     </div>
