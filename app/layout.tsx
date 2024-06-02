ec4553e0bb76b0fa352ce627fe6a1ca771731482
inunice <ammanguan@up.edu.ph>
2024-06-02 21:23:58 +0800

    feat: add logo to page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/icon.png" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
